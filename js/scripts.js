506c3a86a2ddb5fa3c3fd029827dcd79ca9974a8
zeroKoma <zeroKoma@gmail.com>
2025-02-01 21:52:34 +0100

    Input change video background url

--- js/scripts.js
+++ js/scripts.js
@@ -43,6 +43,8 @@ document.addEventListener("DOMContentLoaded", () => {
     <path d="M6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06M6 5.04 4.312 6.39A.5.5 0 0 1 4 6.5H2v3h2a.5.5 0 0 1 .312.11L6 10.96zm7.854.606a.5.5 0 0 1 0 .708L12.207 8l1.647 1.646a.5.5 0 0 1-.708.708L11.5 8.707l-1.646 1.647a.5.5 0 0 1-.708-.708L10.793 8 9.146 6.354a.5.5 0 1 1 .708-.708L11.5 7.293l1.646-1.647a.5.5 0 0 1 .708 0"/>
   </svg>`;
 
+  const videoUrlByDefault = "https://www.youtube.com/watch?v=NIju0uaZue8"
+
   /* FUNCTIONS */
 
   function switchToBreakMode() {
@@ -449,7 +451,6 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   function initApp() {
-    loadVideo()
     resetAccumulatedTimesIfNewDay();
     // Init focus/break sliders
     focusSlider.addEventListener("input", () => {
@@ -548,6 +549,11 @@ document.addEventListener("DOMContentLoaded", () => {
     temp = storedVideoBackgroundSoundVolume ? storedVideoBackgroundSoundVolume : 0;
     setVolumeSliderValue("videoBackgroundSlider", temp);
     setLocalStorageItem("videoBackgroundVolume", temp);
+
+    const storedVideoBackgroundURL = getLocalStorageItem("videoBackgroundURL");
+    temp = storedVideoBackgroundURL ? storedVideoBackgroundURL : videoUrlByDefault;
+    setInitialVideo(temp)
+    setLocalStorageItem("videoBackgroundURL", temp);
 
     const storedalarmSoundVolume = getLocalStorageItem("alarmSoundVolume");
     temp =
--- js/video.js
+++ js/video.js
@@ -1,29 +1,67 @@
-const videoId = "mwf1vgCgMLw";
+let videoId;
+
+function getYouTubeID(url) {
+  try {
+    let urlObj = new URL(url);
+    let match =
+      urlObj.href.match(/[?&]v=([^&]+)/) ||
+      urlObj.href.match(/youtu\.be\/([^?]+)/);
+    return match ? match[1] : null;
+  } catch (error) {
+    return null;
+  }
+}
 
 let player;
 
 function playVideo() {
-    const storedVideoBackgroundSoundVolume = getLocalStorageItem("videoBackgroundVolume");
-    temp = storedVideoBackgroundSoundVolume ? storedVideoBackgroundSoundVolume : 0;
-    setVolumeSliderValue("videoBackgroundSlider", temp);
-    changeVideoSoundVolume(temp);
-    setLocalStorageItem("videoBackgroundVolume", temp);
-    if (player) player.playVideo();
+  const storedVideoBackgroundSoundVolume = getLocalStorageItem(
+    "videoBackgroundVolume"
+  );
+  temp = storedVideoBackgroundSoundVolume
+    ? storedVideoBackgroundSoundVolume
+    : 0;
+  setVolumeSliderValue("videoBackgroundSlider", temp);
+  changeVideoSoundVolume(temp);
+  setLocalStorageItem("videoBackgroundVolume", temp);
+  if (player) player.playVideo();
 }
 
 function stopVideo() {
-    if (player) player.stopVideo();
+  if (player) player.stopVideo();
 }
 
 function pauseVideo() {
-    if (player) player.pauseVideo();
+  if (player) player.pauseVideo();
 }
 
 function changeVideoSoundVolume(newVolume) {
-    if (player) {
-        player.setVolume(newVolume)
-    }
+  if (player) {
+    player.setVolume(newVolume);
   }
+}
+
+document
+  .getElementById("changeVideoIdInput")
+  .addEventListener("click", changeVideo);
+
+function setInitialVideo(url) {
+  videoId = getYouTubeID(url);
+  loadVideo();
+}
+
+function changeVideo() {
+  const url = document.getElementById("videoIdInput").value;
+  if (url && url.length !== 0) {
+    videoId = getYouTubeID(url);
+    if (videoId) {
+      player.destroy();
+      loadVideo();
+      setLocalStorageItem("videoBackgroundURL", url);
+    }
+    document.getElementById("videoIdInput").value = "";
+  }
+}
 
 function loadVideo() {
   var tag = document.createElement("script");
@@ -38,7 +76,6 @@ function loadVideo() {
     rel: 0,
     iv_load_policy: 3,
     cc_load_policy: 1,
-    autoplay:1
   };
 
   options = {
@@ -47,15 +84,14 @@ function loadVideo() {
     videoId: videoId,
     playerVars: playerVars,
     events: {
-        onReady: onPlayerReady,
-      }
+      onReady: onPlayerReady,
+    },
   };
-  window.YT.ready(function() {
-      if (videoId) player = new YT.Player("yt-background", options);
-  })
+  window.YT.ready(function () {
+    if (videoId) player = new YT.Player("yt-background", options);
+  });
 
-  function onPlayerReady(){
-    console.log('Player ready!!!!')
+  function onPlayerReady() {
+    console.log("Player Ready!");
   }
- 
 }
